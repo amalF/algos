1cd7a7c140e0e61c3a0025aa44295fc7f182aaa7
aferiani <amal.feriani@invision.ai>
2019-10-17 18:03:29 -0400

    Improve Linked List implementation

--- data-structures/linkedlist/linkedlist.h
+++ data-structures/linkedlist/linkedlist.h
@@ -1,7 +1,6 @@
 /**
  * Linked List Implementation
- * @author
- *      Amal Feriani
+ * @author Amal Feriani
  */
 
 #pragma once
@@ -12,8 +11,71 @@ namespace ds {
   template <typename T>
     class LinkedList {
       public:
-      LinkedList() : head(nullptr){};
+        class Node{
+          public:
+            const T& value;
+            Node * next;
+            // Constructors
+            Node(const T& data) : value(data), next(nullptr){};
+            Node(const T& data, Node * next) : value(data), next(next){};
+            // Copy constructor
+            Node(const Node & other) : value(other.value), next(other.next){};
+            // Assignment operator
+            Node& operator=(const Node & other){
+              value = other.value;
+              next = other.next;
+              return *this;
+            }
+        };
+
+        // Default constructor
+        LinkedList() : head(nullptr){};
+        // Destructor : deallocate all the nodes
         ~LinkedList(){
+          clear();
+        };
+
+        // Copy constructor
+        LinkedList(const LinkedList<T>& other) : LinkedList() {
+          *this = other;
+        }
+
+        // Assignment constructor
+        LinkedList<T>& operator=(const LinkedList<T>& other){
+          // Clear the current list.
+          clear();
+
+          // We'll walk along the other list and push copies of its data.
+          // (The declaration "const Node*" means a pointer to a const Node,
+          //  not a const pointer to Node.)
+          const Node* cur = other.head;
+          while (cur) {
+            // Push back a copy of this node's data
+            push_back(cur->value);
+            // Iterate
+            cur = cur->next;
+          }
+          return *this;
+        }
+        //
+
+        // Access
+        // These functions enables to see the content of a node but it's not possible
+        // to change the value of a node
+        const T& front() const {
+          if (!head){ throw std::runtime_error("front() called on an empty list");}
+          return head->value;
+        }
+        const T & operator[](int index){
+          return get(index)->value;
+        };
+
+        // Capacity
+        int size() const;
+        bool isEmpty();
+
+        // Modifiers
+        void clear(){
           // Iterate through the list and delete the nodes
           Node * thru = head;
           while (thru != nullptr){
@@ -23,40 +85,48 @@ namespace ds {
             todelete = nullptr;
           }
         };
+        void push_front(const T & data);
+        void insert_at(const T& data, int index);
+        void push_back(const T& data);
+        void pop_front();
+        void remove_at(int index);
 
-        void push(const T & data);
-        void insertAt(const T& data, int index);
-        void append(const T& data);
-
-        void remove(const T& data);
-        void removeAt(int index);
-
-        const T & operator[](int index);
-
-        int size();
-        bool isEmpty();
-
+        // Operations
+        void remove_all(const T& data);
         bool contains(const T& data);
-        
+        bool equals(const LinkedList<T>& other) const;
+        bool operator==(const LinkedList<T>& other) const {
+          return equals(other);
+        }
+        bool operator!=(const LinkedList<T>& other) const {
+          return !equals(other);
+        }
+        void insertOrdered(const T& newData);
+        bool isSorted() const;
+        LinkedList<T> insertionSort() const;
+        std::tuple<LinkedList<T>,LinkedList<T>> splitHalves() const;
+        LinkedList<T> merge(const LinkedList<T>& other) const;
+        LinkedList<T> mergeSort() const;
+        std::ostream& print(std::ostream& os) const;
+
+        friend std::ostream& operator<<(std::ostream& s, const LinkedList<T>& v) {
+          s.put('[');
+          Node * thru = v.head;
+          char comma[3] = {'\0', ' ', '\0'};
+          while (thru) {
+            s << comma << thru->value;
+            comma[0] = ',';
+            thru = thru->next;
+          }
+          return s << ']';
+        }
 
       private:
-        class Node{
-          public:
-            const T& val;
-            Node * next;
-            Node(const T& data) : val(data), next(nullptr){};
-            Node(const T& data, Node * next) : val(data), next(next){};
-            Node(const Node & other){val = other.val2; next = other.next;}
-
-        };
-
-        
         Node * find(const T& data);
         Node * get(int index);
-
-        Node *head;
+        bool _isSorted(Node * headRef) const;
+        Node* head;
         int len = 0;
-
     };
 }
 #include "linkedlist.hpp"
--- data-structures/unittests/testlinkedlist/linkedlist_test.cpp
+++ data-structures/unittests/testlinkedlist/linkedlist_test.cpp
@@ -17,7 +17,7 @@ TEST(LinkedListTest, AddTest){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   for (int i=0;i<10;i++){
-    a1.push(elements[i]);
+    a1.push_front(elements[i]);
   }
   for (int i=0;i<10;i++){
     EXPECT_EQ(a1[i],10-i);
@@ -29,17 +29,17 @@ TEST(LinkedListTest, InsertTest){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   for (int i=0;i<10;i++){
-    a1.push(elements[i]);
+    a1.push_front(elements[i]);
   }
   const int item_a = 50;
   const int item_b = 0;
   const int item_c = 20;
   //insert at the front
-  a1.insertAt(item_b,0);
+  a1.insert_at(item_b,0);
   //insert in the middle
-  a1.insertAt(item_a,5);
+  a1.insert_at(item_a,5);
   //insert at the end
-  a1.insertAt(item_c,12);
+  a1.insert_at(item_c,12);
 
   EXPECT_EQ(a1[0],0);
   EXPECT_EQ(a1[5],50);
@@ -51,33 +51,33 @@ TEST(LinkedListTest, InsertOutOfBoundsTest){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   for (int i=0;i<10;i++){
-    a1.push(elements[i]);
+    a1.push_front(elements[i]);
   }
   const int item_a = 50;
-  ASSERT_THROW(a1.insertAt(item_a,15), std::out_of_range);
+  ASSERT_THROW(a1.insert_at(item_a,15), std::out_of_range);
 }
 
 TEST(LinkedListTest, InsertOutOfBounds2Test){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   for (int i=0;i<10;i++){
-    a1.push(elements[i]);
+    a1.push_front(elements[i]);
   }
   const int item_a = 50;
-  ASSERT_THROW(a1.insertAt(item_a,-1), std::out_of_range);
+  ASSERT_THROW(a1.insert_at(item_a,-1), std::out_of_range);
 }
 
 TEST(LinkedListTest, AppendTest){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   for (int i=0;i<10;i++){
-    a1.push(elements[i]);
+    a1.push_front(elements[i]);
   }
   const int item_a = 50;
   const int item_b = 0;
 
-  a1.append(item_a);
-  a1.append(item_b);
+  a1.push_back(item_a);
+  a1.push_back(item_b);
 
   EXPECT_EQ(a1[11],0);
   EXPECT_EQ(a1[10],50);
@@ -87,7 +87,7 @@ TEST(LinkedListTest, AppendTest){
 TEST(LinkedListTest, ContainsTest){
   LinkedList<char> a1 = LinkedList<char>();
   char chars[] = {'a', 'b', 'c','d','e','g','h'};
-  for (const char &c : chars){a1.append(c);}
+  for (const char &c : chars){a1.push_back(c);}
   EXPECT_EQ(a1.contains('a'),true);
   EXPECT_EQ(a1.contains('c'),true);
   EXPECT_EQ(a1.contains('g'),true);
@@ -97,7 +97,7 @@ TEST(LinkedListTest, ContainsTest){
 TEST(LinkedListTest, GetTest){
   LinkedList<std::string> a1 = LinkedList<std::string>();
   std::string strs[] = {"a", "b", "c","d","e","g","h"};
-  for (const std::string &s : strs){a1.append(s);}
+  for (const std::string &s : strs){a1.push_back(s);}
 
   EXPECT_EQ(a1[0], "a");
   EXPECT_EQ(a1[3], "d");
@@ -107,7 +107,7 @@ TEST(LinkedListTest, GetTest){
 
 TEST(LinkedListTest, RemoveFromEmptyListTest){
   LinkedList<int> a1 = LinkedList<int>();
-  ASSERT_THROW(a1.removeAt(0), std::out_of_range);
+  ASSERT_THROW(a1.remove_at(0), std::out_of_range);
 }
 
 
@@ -115,35 +115,35 @@ TEST(LinkedListTest, RemoveOutOfBoundTest){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3};
   for (const int &elt : elements){
-    a1.push(elt);
+    a1.push_front(elt);
   }
-  ASSERT_THROW(a1.removeAt(3), std::out_of_range);
+  ASSERT_THROW(a1.remove_at(3), std::out_of_range);
 }
 
 TEST(LinkedListTest, RemoveOutOfBoundTest2){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   for (const int &elt : elements){
-    a1.push(elt);
+    a1.push_front(elt);
   }
-  ASSERT_THROW(a1.removeAt(11), std::out_of_range);
+  ASSERT_THROW(a1.remove_at(11), std::out_of_range);
 }
 
 TEST(LinkedListTest, RemoveOutOfBoundTest3){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   for (const int &elt : elements){
-    a1.push(elt);
+    a1.push_front(elt);
   }
 
-  ASSERT_THROW(a1.removeAt(-1), std::out_of_range);
+  ASSERT_THROW(a1.remove_at(-1), std::out_of_range);
 }
 
 TEST(LinkedListTest, RemoveAtTest){
   LinkedList<std::string> a1 = LinkedList<std::string>();
   std::string strs[] = {"a", "b", "c","d","e","g","h"};
-  for (const std::string & s : strs){a1.append(s);}
-  ASSERT_NO_THROW(a1.removeAt(2));
+  for (const std::string & s : strs){a1.push_back(s);}
+  ASSERT_NO_THROW(a1.remove_at(2));
   EXPECT_EQ(a1.size(),6);
 }
 
@@ -151,8 +151,8 @@ TEST(LinkedListTest, RemoveAtTest){
 TEST(LinkedListTest, RemovingTest){
   LinkedList<std::string> a1 = LinkedList<std::string>();
   std::string strs[] = {"a", "b", "c","d","e","g","h"};
-  for (const std::string & s : strs){a1.append(s);}
-  a1.remove("c");
+  for (const std::string & s : strs){a1.push_back(s);}
+  a1.remove_all("c");
   EXPECT_EQ(a1.size(),6);
 }
 
@@ -160,10 +160,10 @@ TEST(LinkedListTest, RemovingTest){
 TEST(LinkedListTest, RemovingWithDuplicatesTest){
   LinkedList<std::string> a1 = LinkedList<std::string>();
   std::string strs[] = {"a", "a", "a","b","b","b","b"};
-  for (const std::string & s : strs){a1.append(s);}
-  a1.remove("b");
+  for (const std::string & s : strs){a1.push_back(s);}
+  a1.remove_all("b");
   EXPECT_EQ(a1.size(),3);
-  a1.remove("a");
+  a1.remove_all("a");
   EXPECT_EQ(a1.isEmpty(), true);
 }
 
@@ -171,24 +171,134 @@ TEST(LinkedListTest, AddAndRemoveTest){
   LinkedList<float> a1 = LinkedList<float>();
   const float elements[] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
 
-  for (const float & f : elements){ a1.append(f);}
-  for (const float & f : elements){ a1.remove(f);}
-  EXPECT_EQ(a1.isEmpty(),true);
-
-  for (const float & f : elements){ a1.push(f);}
-  for (int i=0;i<10;i++){a1.removeAt(0);}
+  for (const float & f : elements){ a1.push_back(f);}
+  for (const float & f : elements){ a1.remove_all(f);}
   EXPECT_EQ(a1.isEmpty(),true);
 }
 
+TEST(LinkedListTest, AddAndPopFrontTest){
+  LinkedList<float> a1 = LinkedList<float>();
+  const float elements[] = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
+
+  for (const float & f : elements){ a1.push_front(f);}
+  for (int i=0;i<10;i++){a1.pop_front();}
+  EXPECT_EQ(a1.isEmpty(),true);
+}
 
 TEST(LinkedListTest, GetSizeTest){
   LinkedList<int> a1 = LinkedList<int>();
   const int elements[] = {1,2,3,4,5,6,7,8,9,10};
   int l = 0;
   for (const int & elt : elements){
-    a1.append(elt);
+    a1.push_back(elt);
     l++;
     EXPECT_EQ(a1.size(),l);
   }
+}
+
+TEST(LinkedListTest, InsertOrderedAtFrontTest){
+
+  const int elements [] = {1,2,3,7,49};
+
+  LinkedList<int> list0;
+
+  for (const int & elt : elements){
+    list0.push_back(elt);
+  }
+
+  LinkedList<int> list0_correct = list0;
+  list0_correct.push_front(-100);
+
+  list0.insertOrdered(-100);
+  EXPECT_EQ(list0, list0_correct);
+  EXPECT_EQ(list0.size(), list0_correct.size());
+
+}
+
+TEST(LinkedListTest, InsertOrderedAtEndTest){
+
+  const int elements [] = {1,2,3,7,49};
+  LinkedList<int> list0;
+  for (const int & elt : elements){
+    list0.push_back(elt);
+  }
+  LinkedList<int> list0_correct = list0;
+  list0_correct.push_back(100);
+
+  list0.insertOrdered(100);
+  EXPECT_EQ(list0, list0_correct);
+  EXPECT_EQ(list0.size(), list0_correct.size());
+}
+
+TEST(LinkedListTest, InsertOrderedAtMiddleTest){
+
+  const int elements [] = {1,2,3,7,49};
+  const int expected [] = {1,2,3,5,7,49};
+
+  LinkedList<int> list0;
+  LinkedList<int> list0_correct;
+
+  for (const int & elt : elements){
+    list0.push_back(elt);
+  }
+  for (const int & elt : expected){
+    list0_correct.push_back(elt);
+  }
+  const int item = 5;
+  list0.insertOrdered(item);
+  EXPECT_EQ(list0, list0_correct);
+  EXPECT_EQ(list0.size(), list0_correct.size());
+  EXPECT_EQ(list0[3],5);
+}
+
+TEST(LinkedListTest, InsertOrderedEmptyTest){
+  LinkedList<int> l;
+  auto expectedList = l;
+  const int a = 100;
+  expectedList.push_back(a);
+  auto result = l;
+  result.insertOrdered(a);
+  EXPECT_EQ(result, expectedList);
+  EXPECT_EQ(result.size(), expectedList.size());
+}
+
+TEST(LinkedListTest, IsSortedTest){
+  const int elements [] = {1,2,3,7,49};
+  LinkedList<int> list0;
+  for (const int & elt : elements){
+    list0.push_back(elt);
+  }
+
+  EXPECT_EQ(list0.isSorted(), true);
+
+  const int a = 10;
+  list0.push_back(a);
+  EXPECT_EQ(list0.isSorted(), false);
+}
+
+TEST(LinkedListTest, SplitHalvesEvenSizeTest){
+  const int elements [] = {1,2,3,7,49,5};
+  const int L[] = {1,2,3};
+  const int R[] = {7,49,5};
+  LinkedList<int> list0;
+  LinkedList<int> expectedFirstHalf;
+  LinkedList<int> expectedSecondHalf;
+
+  for (const int & elt : elements){
+    list0.push_back(elt);
+  }
+  for (const int & elt : L){
+    expectedFirstHalf.push_back(elt);
+  }
+
+  for (const int & elt : R){
+    expectedSecondHalf.push_back(elt);
+  }
+
+  auto splits = list0.splitHalves();
+  EXPECT_EQ(std::get<0>(splits).size(), 3);
+  EXPECT_EQ(std::get<1>(splits).size(), 3);
+  EXPECT_EQ(std::get<0>(splits), expectedFirstHalf);
+  EXPECT_EQ(std::get<1>(splits), expectedSecondHalf);
 }
 
