a87bb2018c9b37c675011c2d055a4211b00b3ea9
Amal <amal.feriani@gmail.com>
2019-11-09 16:59:57 -0500

    Fix in add function of BST

--- data-structures/binarysearchtree/binarysearchtree.h
+++ data-structures/binarysearchtree/binarysearchtree.h
@@ -6,35 +6,52 @@
 
 #pragma once
 #include <stdexcept>
+#include <iostream>
+#include <ostream>
+#include <vector>
+#include <stack>
 
 namespace ds{
 	template <typename T>
 		class BSTree{
 
 			private:
-				class Node{
+				class TreeNode{
 					public:
-						Node(T value) : data(value), left(nullptr), right(nullptr){};
-						Node(T value, Node* l, Node* r) : data(value), left(l), right(l){};
-						~Node(){};
+						TreeNode(T value) : data(value), left(nullptr), right(nullptr){};
+						TreeNode(T value, TreeNode* l, TreeNode* r) : data(value), left(l), right(r){};
+						~TreeNode(){};
 						bool isLeafNode(){
 							if ((!left) && (!right)){return true;}
 							else{return false;}
 						}
+						std::vector<TreeNode*> getChildren() const {
+							std::vector<TreeNode*> childrenPtr;
+							if (left){
+								TreeNode* cur = this->left;
+								childrenPtr.push_back(cur);
+							}
+							if (right){
+								TreeNode* curr = this->right;
+								childrenPtr.push_back(right);
+							}
+							return childrenPtr;
+						}
+
 						const T data;
-						Node* left;
-						Node* right;
+						TreeNode* left;
+						TreeNode* right;
 				};
 
-				Node* _root;
+				TreeNode* _root;
 				int height=0;
 				int len=0;
-				bool _add(Node* node, const T value);
+				TreeNode* _add(TreeNode* node, const T value);
 
 
 			public:
 				BSTree() : _root(nullptr){};
-				BSTree(T value) : _root(new Node(value)){};
+				BSTree(T value) : _root(new TreeNode(value)){};
 				~BSTree(){
 					bool success = deleteSubTree(_root);
 					if(!success){throw std::runtime_error("Something wrong happened during deleting the tree");}
@@ -47,45 +64,50 @@ namespace ds{
 					if (_root){return _root->data;}
 				}
 
-				bool deleteSubTree(Node* targetRoot);
-				bool add(const T value);
+				bool deleteSubTree(TreeNode* targetRoot);
+				void add(const T value);
+
+				std::ostream& Print(std::ostream& os) const;
+
+				friend std::ostream& operator<<(std::ostream& os, const BSTree<T>& tree){
+					return tree.Print(os);
+				}
 
 		};
 
 	template <typename T>
-		bool BSTree<T>::add(const T value){
+		void BSTree<T>::add(const T value){
 
 			if (!_root){
-				_root = new Node(value);
+				_root = new TreeNode(value);
 				len++;
-				return true;
+				return;
 			}
-
-			return _add(_root, value);
+			_root = _add(_root, value);
+			len++;
 		}
 
 	template <typename T>
-		bool BSTree<T>::_add(Node* rootNode, const T value){
+		typename BSTree<T>::TreeNode* BSTree<T>::_add(TreeNode* rootNode, const T value){
 			if (!rootNode){
-				rootNode = new Node(value);
-				len++;
-				return true;
+				rootNode = new TreeNode(value);
 			}
-			if (rootNode->data==value){
-				return false;
+			else if (rootNode->data==value){
+				std::cout << "Value already exists"<<std::endl;
 			}
 			else{
 				if(value>rootNode->data){
-					_add(rootNode->right, value);
+					rootNode->right = _add(rootNode->right, value);
 				}
 				else{
-					_add(rootNode->left, value);
+					rootNode->left = _add(rootNode->left, value);
 				}
 			}
+			return rootNode;
 		}
 
 	template <typename T>
-		bool BSTree<T>::deleteSubTree(Node* targetRoot){
+		bool BSTree<T>::deleteSubTree(TreeNode* targetRoot){
 			if (!targetRoot){
 				return true;
 			}
@@ -100,5 +122,96 @@ namespace ds{
 			}
 		}
 
+	template <typename T>
+		std::ostream& BSTree<T>::Print(std::ostream& os) const {
+			const TreeNode* rootNode = this->_root;
+			if(!_root){
+				os << "[empty tree]" << std::endl;
+				return os;
+			}
+
+			std::stack<const TreeNode*> nodesToPrint;
+			nodesToPrint.push(rootNode);
+
+			//std::stack<int> depthStack;
+			//depthStack.push(0);
+
+			std::stack<std::vector<bool>> curMarginStack;
+			curMarginStack.push( std::vector<bool>() );
+			std::stack<std::vector<bool>> trailingMarginStack;
+			trailingMarginStack.push(std::vector<bool>());
+
+			while(!nodesToPrint.empty()){
+
+				const TreeNode* cur = nodesToPrint.top();
+				nodesToPrint.pop();
+				// Pop the current depth for the node being explored.
+				//int curDepth = depthStack.top();
+				//depthStack.pop();
+
+				// Pop the current and trailing margin graphic flags for this node.
+				std::vector<bool> curMargin = curMarginStack.top();
+				curMarginStack.pop();
+				std::vector<bool> trailingMargin = trailingMarginStack.top();
+				trailingMarginStack.pop();
+
+				constexpr int LAST_ROW = 2;
+				for (int row=1; row<=LAST_ROW; row++){
+					for (auto stemIt= curMargin.begin(); stemIt !=curMargin.end(); stemIt++){
+						bool showStem = *stemIt;
+						std::string stemSymbol = "|";
+						if (!showStem){
+							stemSymbol = " ";
+						}
+						bool isLastCol = false;
+						if (stemIt+1 == curMargin.end()){
+							isLastCol = true;
+						}
+
+						if (isLastCol){
+							if (LAST_ROW==row){
+								os << stemSymbol << "_ ";
+							}
+							else if (showStem){
+								os << stemSymbol << std::endl;
+							}
+							else{
+								os << std::endl;
+							}
+						}
+						else{
+							os << stemSymbol << " ";
+						}
+					}
+				}
+				if (cur){
+					os << cur->data << std::endl;
+				}
+				else{
+					os << "[null]" << std::endl;
+				}
+
+				if (cur){
+					std::vector<TreeNode*> childrenPtr = cur->getChildren();
+					//std::cout << childrenPtr.size()<< std::endl;
+					//std::cout << "cur->left" << cur->left << std::endl;
+					//std::cout << "cur->right" << cur->right << std::endl;
+					for (auto it = childrenPtr.rbegin();it!=childrenPtr.rend();it++){
+						const TreeNode* childPtr = *it;
+						//std::cout << "CHildPTR " << childPtr->data << std::endl;
+						nodesToPrint.push(childPtr);
+						//depthStack.push(curDepth+1);
+						auto nextMargin = trailingMargin;
+						nextMargin.push_back(true);
+						curMarginStack.push(nextMargin);
+
+						auto nextTrailingMargin = trailingMargin;
+						nextTrailingMargin.push_back(true);
+						trailingMarginStack.push(nextTrailingMargin);
+					}
+				}
+			}
+			return os;
+		}
 };
 
--- data-structures/unittests/testbinarysearchtree/binarysearchtree_test.cpp
+++ data-structures/unittests/testbinarysearchtree/binarysearchtree_test.cpp
@@ -11,14 +11,21 @@ TEST(BSTTest, isEmptyTest){
 	BSTree<int> bst = BSTree<int>();
 	EXPECT_EQ(bst.isEmpty(),true);
 	EXPECT_EQ(bst.size(),0);
+	std::cout << bst << std::endl;
 }
 
-TEST(BSTTest, addEmptyTreeTest){
+TEST(BSTTest, addTreeTest){
 	BSTree<int> bst;
+	bst.add(5);
+	bst.add(4);
+	bst.add(8);
+	bst.add(1);
+	bst.add(7);
 	bst.add(10);
-	bst.add(2);
-	bst.add(12);
-	EXPECT_EQ(bst.size(),3);
+	bst.add(6);
+	bst.add(9);
+	EXPECT_EQ(bst.size(),8);
 }
+
 
 
